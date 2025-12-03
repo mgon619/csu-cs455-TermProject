2c052e66405f7eaf8fe03cffdf3e9b639ca3d6b0
IlaiShai <ilai.shai6@gmail.com>
2025-12-03 19:59:42 +0200

    formatted lines

--- driver.py
+++ driver.py
@@ -30,30 +30,31 @@ def main():
     df_full = dfone.join(dftwo, ["Station", "Date"], "inner")
     df_full.show()
 
-    #datacolumns = ["Avg Temp","RH Max","Vapor Pressure","Liquid Precip","Gust Speed", "Gust Dir", "Solar Rad"]
-    #condition = " & ".join([f"(col('{c}') != -999)" for c in datacolumns])
+    # datacolumns = ["Avg Temp","RH Max","Vapor Pressure","Liquid Precip","Gust Speed", "Gust Dir", "Solar Rad"]
+    # condition = " & ".join([f"(col('{c}') != -999)" for c in datacolumns])
     df_full = df_full.withColumn("RH Max", col("RH Max").cast("double")) \
-       .withColumn("Vapor Pressure", col("Vapor Pressure").cast("double")) \
-       .withColumn("Liquid Precip", col("Liquid Precip").cast("double")) \
-       .withColumn("Gust Speed", col("Gust Speed").cast("double")) \
-       .withColumn("Gust Dir", col("Gust Dir").cast("double")) \
-       .withColumn("Solar Rad", col("Solar Rad").cast("double")) \
-       .withColumn("Avg Temp", col("Avg Temp").cast("double"))
+        .withColumn("Vapor Pressure", col("Vapor Pressure").cast("double")) \
+        .withColumn("Liquid Precip", col("Liquid Precip").cast("double")) \
+        .withColumn("Gust Speed", col("Gust Speed").cast("double")) \
+        .withColumn("Gust Dir", col("Gust Dir").cast("double")) \
+        .withColumn("Solar Rad", col("Solar Rad").cast("double")) \
+        .withColumn("Avg Temp", col("Avg Temp").cast("double"))
 
     df_fixed = df_full.filter(
-        (col("RH Max") != -999) & 
+        (col("RH Max") != -999) &
         (col("Vapor Pressure") != -999) &
-        (col("Liquid Precip") != -999) & 
-        (col("Gust Speed") != -999) & 
-        (col("Gust Dir") != -999) & 
+        (col("Liquid Precip") != -999) &
+        (col("Gust Speed") != -999) &
+        (col("Gust Dir") != -999) &
         (col("Solar Rad") != -999) &
         (col("Avg Temp") != -999)
     )
     df_fixed.show()
 
-    cutoff = df_fixed.agg(functions.percentile_approx(col("Liquid Precip"), 0.95).alias("cutoff")).collect()[0]["cutoff"]
+    cutoff = df_fixed.agg(functions.percentile_approx(col("Liquid Precip"), 0.95).alias("cutoff")).collect()[0][
+        "cutoff"]
     print(cutoff)
-    #df_highprecip = df_fixed.filter(col("Liquid Precip") >= cutoff)
+    # df_highprecip = df_fixed.filter(col("Liquid Precip") >= cutoff)
     df_fixed = df_fixed.withColumn(
         "High_Precip",
         functions.when(col("Liquid Precip") >= cutoff, 1).otherwise(0)
@@ -66,7 +67,6 @@ def main():
 
     train_df, test_df = df_ml.randomSplit([0.8, 0.2], seed=42)
 
-  
     rf = RandomForestClassifier(labelCol="High_Precip", featuresCol="features", numTrees=100)
     model = rf.fit(train_df)
 
